b050d2e07e0861c2180ed6d2f8309f5a08dede9b
Ryan-Symplectic <ryan@symcsx.com>
2021-11-23 15:03:04 -0600

    Added Currency enum
    (added serde as a dependency)

--- src/addresses.rs
+++ src/addresses.rs
@@ -1,9 +1,43 @@
 use ethers::types::Address;
+use serde::{Serialize, Deserialize};
 
 /// Parse a string into an Address
 pub fn parse_address(addr: &str) -> Address {
     let addr = addr.strip_prefix("0x").unwrap_or(addr);
     addr.parse().unwrap()
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub enum Currency {
+    WETH,
+    USDC,
+    USDT,
+    WBTC,
+    DAI,
+    RPL,
+    SNX,
+    UNI,
+    LINK,
+    OHM,
+    IERC20(Address),
+}
+
+impl Currency {
+    pub fn kovan_address(&self) -> Address {
+        match &self {
+            Currency::WETH => parse_address(kovan::ETH_ERC20_ADDRESS),
+            Currency::USDC => parse_address(kovan::USDC_ERC20_ADDRESS),
+            Currency::USDT => parse_address(kovan::USDT_ERC20_ADDRESS),
+            Currency::WBTC => parse_address(kovan::WBTC_ERC20_ADDRESS),
+            Currency::DAI => parse_address(kovan::DAI_ERC20_ADDRESS),
+            Currency::RPL => parse_address(kovan::RPL_ERC20_ADDRESS),
+            Currency::SNX => parse_address(kovan::SNX_ERC20_ADDRESS),
+            Currency::UNI => parse_address(kovan::UNI_ERC20_ADDRESS),
+            Currency::LINK => parse_address(kovan::LINK_ERC20_ADDRESS),
+            Currency::OHM => parse_address(kovan::OHM_ERC20_ADDRESS),
+            Currency::IERC20(x) => *x,
+        }   
+    }
 }
 
 /// Addresses on the Live Net
