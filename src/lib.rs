76c255463eec230ba02e6442a5cee619bfb6cd2d
Ryan-Symplectic <ryan@symcsx.com>
2021-12-06 01:57:31 -0600

    Removed default unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,3 @@ pub mod addresses;
 pub mod contracts;
 pub mod providers;
 pub mod util;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
